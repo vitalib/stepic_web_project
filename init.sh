a1094a268e17e8de1a7f88601d3f3f70f8e6a431
vitalib <vitali.baranov@gmail.com>
2018-07-18 07:51:41 +0000

    sdlkfj

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn/test
 sudo /etc/init.d/gunicorn restart
